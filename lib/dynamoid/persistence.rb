1712cc7f06d70370cd41e4d4a1537c4938c8bd6e
Pablo Levy <pablo@busolinea.com>
2019-05-14 17:06:10 -0500

    fix table with no namespace

--- lib/dynamoid/persistence.rb
+++ lib/dynamoid/persistence.rb
@@ -21,7 +21,13 @@ module Dynamoid
         table_base_name = options[:name] || base_class.name.split('::').last
                                                       .downcase.pluralize
 
-        @table_name ||= [Dynamoid::Config.namespace.to_s, table_base_name].reject(&:empty?).join('_')
+        table_prefix = if Dynamoid::Config.namespace.nil? then
+          ''
+        else
+          "#{Dynamoid::Config.namespace}_"
+        end
+
+        @table_name ||= "#{table_prefix}#{table_base_name}"
       end
 
       # Creates a table.
